42915099f073d378df0716fb73d7b9973c5516fe
Hao Li <haoli@alauda.io>
2020-10-28 13:59:26 +0800

    Change get router

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -9,35 +9,20 @@ const COOKIE_VALUE = 'Are you ok ?'
 let name = 'chaos1ee'
 
 router.get('/', async ctx => {
-  ctx.cookies.set(COOKIE_NAME, COOKIE_VALUE, {
-    maxAge: 60 * 60 * 1000,
-    secure: true,
-    sameSite: false
-  })
+  if (!ctx.cookies.get(COOKIE_NAME)) {
+    ctx.cookies.set(COOKIE_NAME, COOKIE_VALUE, {
+      maxAge: 60 * 60 * 1000,
+      secure: true,
+      sameSite: false
+    })
+  }
 
-  await ctx.render('index', {
-    name,
-    cookies: [
-      {
-        name: COOKIE_NAME,
-        value: ctx.cookies.get(COOKIE_NAME) || COOKIE_VALUE
-      }
-    ]
-  })
+  await ctx.render('index', { name })
 })
 
 router.get('/get', async ctx => {
-  if (ctx.cookies.get(COOKIE_NAME) === COOKIE_VALUE) {
-    name = ctx.query.name
-
-    ctx.body = {
-      name
-    }
-  } else {
-    ctx.body = {
-      msg: '获取不到cookie'
-    }
-  }
+  name = ctx.query.name
+  ctx.redirect('/')
 })
 
 module.exports = router
