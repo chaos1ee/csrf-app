4cb7e8b7f5bb3de721e6f5e746c99fa0c1d4e0ae
Hao Li <haoli@alauda.io>
2020-10-28 12:33:41 +0800

    Ignore .idea

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ const views = require('koa-views')
 const bodyparser = require('koa-bodyparser')
 
 import registerRouter from './routes/index'
-import logger from './util/logger'
+// import logger from './util/logger'
 
 const https = require('https')
 const fs = require('fs')
 
 const app = new Koa()
+
+const isDev = process.env.NODE_ENV === 'development'
 
 app.use(bodyparser())
 
@@ -25,22 +27,20 @@ app.use(
 
 app.use(registerRouter())
 
-const getTlsOptions = () => {
-  return process.env.NODE_ENV === 'development' ? {
-      key: fs.readFileSync(resolve(__dirname, './keys/', 'server.key')),
-      cert: fs.readFileSync(resolve(__dirname, './keys/', 'server.crt'))
-    }
-    : {
-      key: fs.readFileSync(resolve('/keys/', 'server.key')),
-      cert: fs.readFileSync(resolve('/keys/', 'server.crt'))
-    }
-}
+
+const getTlsOptions = () => isDev ? {
+    key: fs.readFileSync(resolve(__dirname, './keys/', 'server.key')),
+    cert: fs.readFileSync(resolve(__dirname, './keys/', 'server.crt'))
+  }
+  : {
+    key: fs.readFileSync(resolve('/keys/', 'server.key')),
+    cert: fs.readFileSync(resolve('/keys/', 'server.crt'))
+  }
+
 
 https
   .createServer(
     getTlsOptions(),
     app.callback()
   )
-  .listen(Number(process.env.PORT), '0.0.0.0', () => {
-    logger.info(`Server is running on ${process.env.PORT}`)
-  })
+  .listen(Number(process.env.PORT), '0.0.0.0')
